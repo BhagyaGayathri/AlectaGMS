d59ae2bdbd08a15db06bb3ffba580ff67b563f70
Bhagya Gayathri <bhagyah4@gmail.com>
2013-10-01 18:54:57 +0530

    Second commit

---
+++ AlectaGMS/src/alectagms/Accountant.java
@@ -0,0 +1,8 @@
+package alecta;
+
+/*
+ * Accountant
+ */
+public class Accountant {
+    
+}
---
+++ AlectaGMS/src/alectagms/SalaryAdministrationSetter.java
@@ -0,0 +1,13 @@
+package alecta;
+
+/*
+ * This sets the salary details entered by the administrator
+ * Basic Salary
+ * OT1
+ * OT2
+ * etc.
+ */
+public class SalaryAdministrationSetter {
+    
+    
+}
